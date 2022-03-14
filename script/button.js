93f8e8f1ebef45f72abf907ac2178e75401a05ed
Junqi Wu <wujunqi@tju.edu.cn>
2022-03-14 19:50:27 +0800

    delete some file

--- script/button.js
+++ script/button.js
@@ -7,36 +7,10 @@ d3.select('#imageWindow .close')
         d3.select('#fsimg').style('display', 'none');
     });
 
-// d3.select('#fsimg .close')
-//     .on('click', function (event) {
-//         d3.select('#fsimg').style('display', 'none');
-//     });
-
-
-// d3.select('#amplify')
-//     .on('click', function (event) {
-//         let width = parseFloat(d3.select('#fsimg').style('width').replace('px', "")),
-//             height = parseFloat(d3.select('#fsimg').style('height').replace('px', ""));
-//
-//         d3.select('#fsimg')
-//             .style('width', `${width + 100}px`)
-//             .style('height', `${height + 100}px`);
-//     });
-//
-// d3.select('#narrow')
-//     .on('click', function (event) {
-//         let width = parseFloat(d3.select('#fsimg').style('width').replace('px', "")),
-//             height = parseFloat(d3.select('#fsimg').style('height').replace('px', ""));
-//
-//         d3.select('#fsimg')
-//             .style('width', `${width - 100}px`)
-//             .style('height', `${height - 100}px`);
-//     });
-
 let isPin = false;
-d3.select('#map')
-    .on('dblclick', function (event) {
-        if (!isPin) {
+d3.select('body')
+    .on('keypress', function (event) {
+        if (event.keyCode === 32 && !isPin) {
             isPin = true;
             mapboxSvg.on('mousemove', null);
         } else {
@@ -44,6 +18,14 @@ d3.select('#map')
             mapboxSvg.on('mousemove', event => glyphMousemove(filterData, glyph_g, sunburst_node_g, sunburst_label_g, event, 'map', map));
             closeImage();
         }
+        // if (!isPin) {
+        //     isPin = true;
+        //     mapboxSvg.on('mousemove', null);
+        // } else {
+        //     isPin = false;
+        //     mapboxSvg.on('mousemove', event => glyphMousemove(filterData, glyph_g, sunburst_node_g, sunburst_label_g, event, 'map', map));
+        //     closeImage();
+        // }
     });
 
 let dragx = 0, dragy = 0;
@@ -69,26 +51,3 @@ d3.select('#imageWindow .drag')
                     .style('transform', `translate(${x - dx}px, ${y - dy}px)`);
             })
     );
-
-// d3.select('#fsimg')
-//     .call(
-//         d3.drag()
-//             .on('start', function (event) {
-//                 dragx = event.sourceEvent.clientX;
-//                 dragy = event.sourceEvent.clientY;
-//             })
-//             .on('drag', function (event) {
-//                 let x = $('#fsimg').position().left,
-//                     y = $('#fsimg').position().top,
-//                     dx = dragx - event.sourceEvent.clientX,
-//                     dy = dragy - event.sourceEvent.clientY;
-//                     dragx = event.sourceEvent.clientX;
-//                     dragy = event.sourceEvent.clientY;
-//
-//                 d3.select('#imageWindow')
-//                     .style('z-index', 5000)
-//                 d3.select('#fsimg')
-//                     .style('z-index', 5001)
-//                     .style('transform', `translate(${x - dx}px, ${y - dy}px)`);
-//             })
-//     );
